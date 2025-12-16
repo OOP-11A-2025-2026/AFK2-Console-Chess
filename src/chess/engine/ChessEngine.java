61b9a5b7fa38188a2d3c2712ff8b3dd8d83eeae8
Kristiyan Kobarelov <kobarelov.k@gmail.com>
2025-12-16 09:48:47 +0200

    Chess Engine Interface

--- src/chess/engine/ChessEngine.java
+++ src/chess/engine/ChessEngine.java
@@ -1,5 +1,29 @@
 package chess.engine;
 
-public class ChessEngine {
-    
+import chess.core.Board;
+import chess.core.Color;
+
+import java.io.IOException;
+
+/**
+ * Generic interface for anything that can produce chess moves (Stockfish, random bot, minimax bot, etc.)
+ */
+public interface ChessEngine extends AutoCloseable {
+
+    /** Start/init resources (process, threads, etc.) */
+    void start() throws IOException;
+
+    /** Optional difficulty controls (implement if supported). */
+    default void setSkillLevel(int level) throws IOException { /* optional */ }
+
+    /** Returns best move in UCI format: "e2e4", "g8f6", "a7a8q", etc. */
+    String bestMove(Board board, Color sideToMove, int depth) throws IOException;
+
+    /** Stop/cleanup resources */
+    void stop() throws IOException;
+
+    @Override
+    default void close() throws IOException {
+        stop();
+    }
 }
